dffec82c03365af80e42d456002f34835c4a637f
AndrewZinc <107150022+AndrewZinc@users.noreply.github.com>
2022-09-08 15:05:09 -0400

    Updated the general queries file.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -131,14 +131,45 @@ select * from dept_info
 where dept_name in ('Development','Sales')
 order by dept_name, emp_no ASC;
 	
+select e.emp_no, e.first_name, e.last_name, e.hire_date,
+	t.title, t.from_date, t.to_date
+--into retirement_titles
+from employees as e
+left join titles as t
+	on (e.emp_no = t.emp_no)
+where (e.birth_date between '1952-01-01' and '1955-12-31');
+--and (e.hire_date between '1985-01-01' and '1988-12-31');	
 	
-select * from dept_info;
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+--INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER By emp_no ASC, to_date DESC;	
+
+
+
+select count(title), title
+into retiring_titles
+from unique_titles
+group by title
+order by count(title) desc;
 	
-	
-	
-	
-	
-	
-	
-	
-	
+select * from retiring_titles
+
+select distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date, t.title
+into mentorship_eligibility
+from employees as e
+join dept_emp as de
+	on (e.emp_no = de.emp_no)
+join titles as t
+	on (e.emp_no = t.emp_no)
+where (de.to_date = '9999-01-01') and
+	(e.birth_date between '1965-01-01' and '1965-12-31')
+order by e.emp_no;
+
+
